ae2dd5d8f9eb7ad8aec7e922236efd8879468799
nicov2000 <nicolasvarg00@gmail.com>
2023-09-24 15:48:30 -0300

    feat: update helpers file

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,3 +3,12 @@ export const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 // Equal check (does not work with functions or symbol as inputs)
 export const equal = (a, b) => JSON.stringify(a) === JSON.stringify(b)
+
+export const isJsonString = (str) => {
+  try {
+    JSON.parse(str)
+  } catch (e) {
+    return false
+  }
+  return true
+}
